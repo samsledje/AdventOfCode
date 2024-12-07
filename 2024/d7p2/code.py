06d9cdc78849a72d13d318a599a5fa049667383e
Samuel Sledzieski <samsledje@gmail.com>
2024-12-07 18:59:13 -0500

    okay actually optimized now-- thx lehman for reverse direction tip

--- 2024/d7p2/code.py
+++ 2024/d7p2/code.py
@@ -44,27 +44,32 @@ def safe_eval_l2r(expr, soln):
 def np_cat(a,b):
     return int(str(a)+str(b))
 
-def recurse_build(vals):
+def recurse_build(vals, soln):
     if len(vals) == 1:
-        return [[vals[0]]]
+        return [vals[0]]
     else:
-        curr_value = vals[0]
-        running_vals = recurse_build(list(vals[1:]))
+        curr_value = vals[-1]
+        running_vals = recurse_build(list(vals[:-1]), soln)
         new_vals = []
         for v in running_vals:
             try:
-                new_vals.append([curr_value, np.add]+v)
-                new_vals.append([curr_value, np.multiply]+v)
-                new_vals.append([curr_value, np_cat]+v)
+                if curr_value+v <= soln:
+                    new_vals.append(curr_value+v)
+                if curr_value*v <= soln:
+                    new_vals.append(curr_value*v)
+                if np_cat(v, curr_value) <= soln:
+                    new_vals.append(np_cat(v, curr_value))
             except TypeError:
                 raise TypeError
         return new_vals      
 
 
 def test_equation(soln, vals):
-    soln_space = [safe_eval_l2r(v, soln) for v in recurse_build(vals)]
+    soln_expr = recurse_build(vals, soln)
+    # print(soln, vals,(soln in soln_expr), len(vals), 3**(len(vals)-1), len(soln_expr), soln_expr)
+    # soln_space = [safe_eval_l2r(v, soln) for v in soln_expr]
     # print(soln, soln_space)
-    return (soln in soln_space)
+    return (soln in soln_expr)
 
 if __name__ == "__main__":
     
