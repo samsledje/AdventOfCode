f476c985990a40a3abe663d1fa461095bf75e0f4
Samuel Sledzieski <samsledje@gmail.com>
2025-12-08 17:25:41 +0100

    2025/d8

---
+++ 2025/d8p2/code.py
@@ -0,0 +1,79 @@
+import sys
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
+
+
+def unravel(idx, num_items):
+    x = idx // num_items
+    y = idx % num_items
+    return (x, y)
+
+
+if __name__ == "__main__":
+    box_coords = []
+    circuits = []
+    boxes_assigned = {}
+
+    with open(sys.argv[1], "r") as f:
+        for line in f:
+            box_coords.append([int(i) for i in line.strip().split(",")])
+
+    box_coords = np.array(box_coords)
+    num_boxes = box_coords.shape[0]
+
+    distances = pdist(box_coords)
+    distances_sq = squareform(distances)
+
+    upper = np.triu(distances_sq, 1)
+    idx = np.argsort(upper, axis=None)
+    row, col = np.unravel_index(idx, upper.shape)
+    num_zero = int((num_boxes * (num_boxes - 1)) / 2) + num_boxes
+    ordered_pairs = np.vstack([row, col]).T[num_zero:, :]
+
+    for i, (p0, p1) in enumerate(ordered_pairs):
+        p0, p1 = int(p0), int(p1)
+        print(
+            f"[{i}] Pair {p0}, {p1} joins {box_coords[p0]} with {box_coords[p1]} (distance {distances_sq[p0, p1]})"
+        )
+        if (p0 in boxes_assigned) and (p1 in boxes_assigned):
+            p0_circuit = boxes_assigned[p0]
+            p1_circuit = boxes_assigned[p1]
+            joint_circuit = p0_circuit.union(p1_circuit)
+            for i_ in joint_circuit:
+                boxes_assigned[i_] = joint_circuit
+        elif p0 in boxes_assigned:
+            existing_circuit = boxes_assigned[p0]
+            existing_circuit.add(p1)
+            for i_ in existing_circuit:
+                boxes_assigned[i_] = existing_circuit
+        elif p1 in boxes_assigned:
+            existing_circuit = boxes_assigned[p1]
+            existing_circuit.add(p0)
+            for i_ in existing_circuit:
+                boxes_assigned[i_] = existing_circuit
+        else:
+            new_circuit = set([p0, p1])
+            boxes_assigned[p0] = new_circuit
+            boxes_assigned[p1] = new_circuit
+
+        circuits = set([tuple(i) for i in boxes_assigned.values()])
+        num_circuits = len(circuits)
+
+        print(f"[{i}] {len(circuits)} circuits")
+
+        if (num_circuits == 1) and (len(list(circuits)[0]) == num_boxes):
+            break
+
+        # print(boxes_assigned)
+
+    p0_coords = box_coords[p0]
+    p1_coords = box_coords[p1]
+    print(p0_coords[0] * p1_coords[0])
+
+    # circuits = set([tuple(i) for i in boxes_assigned.values()])
+    # sorted_circuits = sorted(list(circuits), key=lambda x: len(x), reverse=True)
+    # # print(sorted_circuits)
+    # circuit_lengths = [len(i) for i in sorted_circuits]
+
+    # # Result
+    # print(np.prod(circuit_lengths[:3]))
